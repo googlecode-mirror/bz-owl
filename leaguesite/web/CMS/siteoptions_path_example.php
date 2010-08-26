8fdb54329f4403ce509a59853d1aa84a200f92ea
ts <ts@68b30ebe-0d82-dabe-3d00-e961b679a46f>
2010-08-26 16:43:51 +0000

    provide better formatted info

--- leaguesite/web/CMS/siteoptions_path_example.php
+++ leaguesite/web/CMS/siteoptions_path_example.php
@@ -6,5 +6,8 @@
 	// do the same for the siteoptions example
 	
 	// __FILE__ is a PHP 5 constant that points to the current file (in this case siteinfo.php)
+	// dirname(__FILE__) gets the directory name where this file resides (CMS/)
+	// the other dirname's work like ../ in the shell.
+	// this example points to ../../siteoptions.php
 	require_once (realpath(dirname(dirname(dirname(__FILE__))) . '/siteoptions.php'));
 ?>
