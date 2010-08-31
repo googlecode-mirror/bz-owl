500627d631357130c663cea4fc4e6ada72003e3f
ts <ts@68b30ebe-0d82-dabe-3d00-e961b679a46f>
2010-08-31 13:40:02 +0000

    rename Test42 to 42, begin outsourcing logic from Players/index.php (first step: editing to Players/edit.php)

--- trunk/leaguesite/web/Config/index.php
+++ trunk/leaguesite/web/Config/index.php
@@ -147,7 +147,7 @@
 <p>Theme:
 	<select name="stylesheet">
 <?php
-	$styles = array('White', 'Snow', 'Eierschale', 'BradTest', 'Test42');
+	$styles = array('White', 'Snow', 'Eierschale', 'BradTest', '42');
 	
 	foreach ($styles AS $s) {
 		echo '<option value="'.$s.'"'.($stylesheet==$s?' selected="selected"':'').'>'.urldecode($s)."</option>\n";
