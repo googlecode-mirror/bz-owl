87ef9c55a8e2267698ba8bec38e2bee3d24d4c51
toughshooter <toughshooter@googlemail.com>
2011-02-13 14:40:40 +0000

    remove usage of baseaddress() in global namespace from menus

--- leaguesite/web/CMS/classes/settings_path_example.php
+++ leaguesite/web/CMS/classes/settings_path_example.php
@@ -1,17 +1,17 @@
 <?php
-	// this is only a siteoptions_path example file
+	// this is only a settings_path example file
 	
 	// copy it to the path specified within siteinfo.php and
 	// edit the return values appropriately to get the site running
-	// do the same for the siteoptions example
+	// do the same for the settings example
 	
-	// you should not have your siteoptions.php in your webserver dir because of security concerns.
+	// you should not have your settings.php in your webserver dir because of security concerns.
 	
 	// __FILE__ is a PHP 5 constant that points to the current file (in this case siteinfo.php)
 	// dirname(__FILE__) gets the directory name where this file resides (CMS/)
 	// the other dirname's work like ../ in the shell.
-	// this example points to ../../siteoptions.php
+	// this example points to ../../settings.php
 	// you could also use an absolute path
-	// 	require_once ('/absolute/path/to/siteoptions.php');
+	// 	require_once ('/absolute/path/to/settings.php');
 	require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/settings.php';
 ?>
--- leaguesite/web/styles/Eierschale/menu.php
+++ leaguesite/web/styles/Eierschale/menu.php
@@ -1,9 +1,10 @@
 <?php
 	class menu
 	{
-		
 		function writeLink($folder, $title, $current=false, $unread=false)
 		{
+			global $config;
+			
 			$link = '<li>';
 			if (!$current)
 			{
@@ -12,10 +13,10 @@
 				{
 					$link .= 'class="unread" ';
 				}
-				$link .= 'href="' . (baseaddress() . $folder) . '">';
+				$link .= 'href="' . ($config->value('baseaddress') . $folder) . '">';
 			} elseif (count($_GET) > 0)
 			{
-				$link .= '<a class="current_nav_entry" href="' . (baseaddress() . $folder) . '">';
+				$link .= '<a class="current_nav_entry" href="' . ($config->value('baseaddress') . $folder) . '">';
 			}
 			$link .= $title;
 			if (!$current || (count($_GET) > 0))
@@ -47,23 +48,21 @@
 			if ($user->getID() > 0)
 			{
 				// the execution of the query is not that time critical and it happens often -> LOW_PRIORITY
-				$query = ('UPDATE LOW_PRIORITY `online_users` SET `last_activity`='
-						  . sqlSafeStringQuotes(date('Y-m-d H:i:s')) . ' WHERE `playerid`=' . sqlSafeStringQuotes($user->getID()));
-				$db->SQL($query);
+				$query = $db->prepare('UPDATE LOW_PRIORITY `online_users` SET `last_activity`=?'
+						  . ' WHERE `playerid`=?');
+				$db->execute($query, array(date('Y-m-d H:i:s'), $user->getID()));
 				
 				// are there unread messages?
-				// are there unread messages?
-				$query = ('SELECT `id` FROM `messages_users_connection` WHERE `msg_status`='
-						  . sqlSafeStringQuotes('new')
-						  . ' AND `playerid`=' . sqlSafeStringQuotes($user->getID())
+				$query = $db->prepare('SELECT `id` FROM `messages_users_connection` WHERE `msg_status`=?'
+						  . ' AND `playerid`=?'
 						  . ' LIMIT 1');
-				$result = $db->SQL($query);
-				$rows = (int) mysql_num_rows($result);
+				
+				$result = $db->execute($query, array('new', $user->getID()));;
+				$rows = $query->rowCount();
 				if ($rows > 0)
 				{
 					$unread_messages = true;
 				}
-				mysql_free_result($result);
 			} else
 			{
 				$logged_in = false;
@@ -95,11 +94,11 @@
 					$menu[] = '<li>Home</li>' . "\n";
 				} else
 				{
-					$menu[] = '<li><a class="current_nav_entry" href="' . (baseaddress()) . '">Home</a></li>' . "\n";
+					$menu[] = '<li><a class="current_nav_entry" href="' . ($config->value('baseaddress')) . '">Home</a></li>' . "\n";
 				}
 			} else
 			{
-				$menu[] = '<li><a href="' . baseaddress() . '">Home</a></li>' . "\n";
+				$menu[] = '<li><a href="' . $config->value('baseaddress') . '">Home</a></li>' . "\n";
 			}
 			
 			if ((isset($_SESSION['user_logged_in'])) && ($_SESSION['user_logged_in']))
