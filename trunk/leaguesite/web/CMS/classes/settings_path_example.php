30199e1669296cc804e3441aa0551a8fb9be4374
toughshooter@googlemail.com <toughshooter@googlemail.com@68b30ebe-0d82-dabe-3d00-e961b679a46f>
2011-02-22 13:04:11 +0000

    forgot to rename the file in comments

--- trunk/leaguesite/web/CMS/classes/settings_path_example.php
+++ trunk/leaguesite/web/CMS/classes/settings_path_example.php
@@ -1,13 +1,13 @@
 <?php
 	// this is only a settings_path example file
 	
-	// copy it to the path specified within siteinfo.php and
+	// copy it to the path specified within settings.php and
 	// edit the return values appropriately to get the site running
 	// do the same for the settings example
 	
 	// you should not have your settings.php in your webserver dir because of security concerns.
 	
-	// __FILE__ is a PHP 5 constant that points to the current file (in this case siteinfo.php)
+	// __FILE__ is a PHP 5 constant that points to the current file (in this case settings.php)
 	// dirname(__FILE__) gets the directory name where this file resides (CMS/)
 	// the other dirname's work like ../ in the shell.
 	// this example points to ../../settings.php
