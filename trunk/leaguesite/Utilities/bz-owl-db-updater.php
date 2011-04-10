6ffa159351b5f6796027d06525a52bc3dc105cfc
toughshooter@gmail.com <toughshooter@gmail.com@68b30ebe-0d82-dabe-3d00-e961b679a46f>
2011-04-10 13:18:02 +0000

    oops, was in the wrong dir, converting function was not commited

--- trunk/leaguesite/Utilities/bz-owl-db-updater.php
+++ trunk/leaguesite/Utilities/bz-owl-db-updater.php
@@ -16,6 +16,12 @@
 	require_once (dirname(dirname(__FILE__)) . '/web/CMS/site.php');
 	$site = new site();
 	
+	// do not run if site is in production mode
+	if (!$config->value('maintenance.now') || !$config->value('maintenance.updateDB'))
+	{
+		exit('Can only update DB if live website is down for maintenance.' . "\n");
+	}
+	
 	
 	// assume if table CMS does not exist db version is oldest, newest otherwise
 	$query = $db->prepare('SHOW COLUMNS FROM `misc_data` WHERE `Field`=?');
@@ -30,7 +36,7 @@
 		$rows = $db->fetchAll($query);
 		$db->free($query);
 		
-		$dbVersion = (count($rows) < 1) ? MAX_DB_VERSION : 0;
+		$dbVersion = (count($rows) > 1) ? MAX_DB_VERSION : 0;
 	} else
 	{
 		$query = $db->SQL('SELECT `db.Version` FROM `misc_data` LIMIT 1');
@@ -51,11 +57,11 @@
 	$error = false;
 	while ($dbVersion < MAX_DB_VERSION && $error === false)
 	{
-		updateDB($dbVersion);
+		updateDB($dbVersion, $error);
 	}
-	echo ($error === false) ? ('The used database (' . $config->value('dbName') . ') is up to date (version ' . $dbVersion . ').' . "\n") : $error;
-	return;
-	
+	echo ($error === false) ? ('The used database (' . $config->value('dbName')
+							   . ') is up to date (version ' . $dbVersion . ').' . "\n") : $error;
+	exit();
 	
 	
 	function updateDB(&$version, &$error)
@@ -66,11 +72,72 @@
 		{
 			echo('Updating...' . "\n");
 			
+			$updateVersionFunction = 'updateVersion' . $version;
+			$updateVersionFunction();
 			// TODO: update code here
 			// TODO: if non-recoverable error occurs, write it into $error
 			
 			// update counter
 			$version++;
 		}
+	}
+	
+	function updateVersion0()
+	{
+		global $db;
+		
+		// PM DB changes
+		echo('renaming PM tables' . "\n");
+		$db->SQL('RENAME TABLE `messages_storage` TO `pmSystem.Msg.Storage`');
+		$db->SQL('CREATE TABLE `pmSystem.Msg.Recipients.Teams` (
+				 `id` int(11) NOT NULL AUTO_INCREMENT,
+				 `msgid` int(11) unsigned DEFAULT NULL,
+				 `teamid` int(11) unsigned DEFAULT NULL,
+				 PRIMARY KEY (`id`)
+				 ) ENGINE=InnoDB DEFAULT CHARSET=utf8');
+		$db->SQL('CREATE TABLE `pmSystem.Msg.Recipients.Users` (
+				 `id` int(11) NOT NULL AUTO_INCREMENT,
+				 `msgid` int(11) unsigned DEFAULT NULL,
+				 `userid` int(11) unsigned DEFAULT NULL,
+				 PRIMARY KEY (`id`)
+				 ) ENGINE=InnoDB DEFAULT CHARSET=utf8');
+		
+		// transform data to new format
+		$convertRecipientTeam=$db->prepare('INSERT INTO `pmSystem.Msg.Recipients.Teams` (`msgid`,`teamid`) VALUES(?,?)');
+		$convertRecipientPlayer=$db->prepare('INSERT INTO `pmSystem.Msg.Recipients.Users` (`msgid`,`userid`) VALUES(?,?)');
+		
+		echo('converting PM recipients to pmSystem.Msg.Recipients.Teams and pmSystem.Msg.Recipients.Users' . "\n"  . '.');
+		$time = time();
+		$query = $db->SQL('SELECT * FROM `pmSystem.Msg.Storage`');
+		while($row = $db->fetchRow($query))
+		{
+			// one progress dot every 5 seconds
+			if (time() - $time > 5)
+			{
+				echo('.');
+				$time = time();
+			}
+			
+			if (isset($row['from_team']) && strcasecmp($row['from_team'], '1') === 0)
+			{
+				$db->execute($convertRecipientTeam, array($row['id'], $row['recipients']));
+				$db->free($convertRecipientTeam);
+			} else
+			{
+				$recipients = explode(' ', $row['recipients']);
+				foreach($recipients AS $recipient)
+				{
+					$db->execute($convertRecipientPlayer, array($row['id'], $recipient));
+					$db->free($convertRecipientPlayer);
+				}
+				unset($recipient);
+			}
+		}
+		echo("\n" . 'done converting PM recipients to v1' . "\n");
+		$db->free($query);
+		
+		echo('altering connecting tables between message and users' . "\n");
+		$db->SQL('RENAME TABLE `messages_users_connection` TO `pmSystem.Msg.Users`');
+		$db->SQL('ALTER TABLE `pmSystem.Msg.Users` DROP `id`');
 	}
 ?>
