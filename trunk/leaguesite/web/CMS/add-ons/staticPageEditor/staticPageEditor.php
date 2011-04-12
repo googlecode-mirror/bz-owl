bcc5d39829b596ad27799e1a44549ee466114f86
toughshooter@gmail.com <toughshooter@gmail.com@68b30ebe-0d82-dabe-3d00-e961b679a46f>
2011-04-12 11:04:20 +0000

    remove old static pages, let the add-on handle it all

--- trunk/leaguesite/web/CMS/add-ons/staticPageEditor/staticPageEditor.php
+++ trunk/leaguesite/web/CMS/add-ons/staticPageEditor/staticPageEditor.php
@@ -2,8 +2,9 @@
 	class staticPageEditor
 	{
 		private $editor;
+		private $path;
 		public $randomKeyName = 'staticPageEditor';
-
+		
 		function __construct($title, $path)
 		{
 			global $entry_edit_permission;
@@ -17,14 +18,15 @@
 			{
 				$tmpl->noTemplateFound();	// does not return
 			}
-
-			$tmpl->assign('title', $this->page_title);
-
+			
+			$this->path = $path;
+			$tmpl->assign('title', $title);
+			
 			// FIXME: fallback to default permission name until add-on system is completly implemented
 			$entry_edit_permission = 'allow_edit_static_pages';
-
+			
 			require_once (dirname(dirname(dirname(__FILE__))) . '/classes/editor.php');
-
+			
 			if ($user->getPermission($entry_edit_permission) && isset($_GET['edit']))
 			{
 				// remove the slashes if magic quotes are sadly on
@@ -42,32 +44,32 @@
 				$tmpl->display();
 				die();
 			}
-
+			
 			// user looks at page in read mode
 			if ($user->getPermission($entry_edit_permission))
 			{
 				$tmpl->assign('showEditButton', true);
 			}
 			$tmpl->assign('content', $this->readContent($path, $author, $last_modified, false));
-
+			
 			// done, display page
 			$tmpl->display();
 		}
-
-
+		
+		
 		function sanityCheck(&$confirmed)
 		{
 			global $entry_edit_permission;
 			global $user;
 			global $tmpl;
-
+			
 			if (!$user->getPermission($entry_edit_permission))
 			{
 				// editing cancelled due to missing user permission
 				$confirmed = 0;
 				return 'noperm';
 			}
-
+			
 			// no need to check for a key match if no content was supplied
 			if (($confirmed > 0) && !$this->randomKeyMatch($confirmed))
 			{
@@ -75,28 +77,28 @@
 				$confirmed = 0;
 				return 'nokeymatch';
 			}
-
+			
 			return true;
 		}
-
+		
 		function insertEditText($readonly=false)
 		{
 			global $tmpl;
 			global $author;
 			global $last_modified;
 			global $config;
-
+			
 			if ($readonly || isset($_POST['confirmationStep']))
 			{
 				$content = $_POST['staticContent'];
 			} elseif (isset($_GET['edit']))
 			{
-				$content = $this->readContent($this->page_title, $author, $last_modified, true);
+				$content = $this->readContent($this->path, $author, $last_modified, true);
 			} else
 			{
 				$content = 'Replace this text with the page content.';
 			}
-
+			
 			switch($readonly)
 			{
 				case true:
@@ -109,7 +111,7 @@
 						$tmpl->assign('contentPreview',  htmlent($content));
 					}
 					break;
-
+				
 				default:
 					$tmpl->assign('rawContent', htmlspecialchars($content
 																 , ENT_COMPAT, 'UTF-8'));
@@ -118,11 +120,11 @@
 					break;
 			}
 		}
-
+		
 		function randomKeyMatch(&$confirmed)
 		{
 			global $site;
-
+			
 			$randomKeyValue = '';
 			$randomKeyName = '';
 
@@ -135,26 +137,26 @@
 					$randomKeyValue = html_entity_decode($_POST[$randomKeyName]);
 				}
 			}
-
+			
 			return $randomkeysmatch = $site->validateKey($randomKeyName, $randomKeyValue);
 		}
-
+		
 		function readContent($path, &$author, &$last_modified, $raw=false)
 		{
 			global $site;
 			global $db;
 			global $config;
-
+			echo $path;
 			// initialise return variable so any returned value will be always in a defined state
 			$content = '';
 			if (!$raw)
 			{
 				$content = 'No content available yet.';
 			}
-
-			$query = $db->prepare('SELECT * FROM `static_pages` WHERE `page_name`=? LIMIT 1');
+			
+			$query = $db->prepare('SELECT * FROM `static_pages` WHERE `page`=? LIMIT 1');
 			$result = $db->execute($query, $path);
-
+			
 			// process query result array
 			while ($row = $db->fetchRow($query))
 			{
@@ -169,34 +171,34 @@
 				}
 			}
 			$db->free($query);
-
+			
 			return $content;
 		}
-
+		
 		function writeContent(&$content)
 		{
 			global $config;
 			global $user;
 			global $tmpl;
 			global $db;
-
+			
 			if (strcmp($content, '') === 0)
 			{
 				// empty content
-				$query = $db->prepare('DELETE FROM `static_pages` WHERE `page_name`=?');
-				$db->execute($query, $this->page_title);
+				$query = $db->prepare('DELETE FROM `static_pages` WHERE `page`=?');
+				$db->execute($query, $this->path);
 				$db->free($query);
 				return;
 			}
-
-			$query = $db->prepare('SELECT `id` FROM `static_pages` WHERE `page_name`=? LIMIT 1');
-			$db->execute($query, $this->page_title);
-
+			
+			$query = $db->prepare('SELECT `id` FROM `static_pages` WHERE `page`=? LIMIT 1');
+			$db->execute($query, $this->path);
+			
 			// number of rows
 			$rows = $db->rowCount($query);
 			$db->free($query);
 			$date_format = date('Y-m-d H:i:s');
-
+			
 			$args = array($user->getID(), $date_format, $content);
 			if ($config->value('bbcodeLibAvailable'))
 			{
@@ -205,14 +207,14 @@
 			{
 				$args[] = $content;
 			}
-			$args[] = $this->page_title;
-
+			$args[] = $this->path;
+			
 			if ($rows < ((int) 1))
 			{
 				// no entry in table regarding current page
 				// thus insert new data
 				$query = $db->prepare('INSERT INTO `static_pages`'
-						. ' (`author`, `last_modified`, `raw_content`, `content`, `page_name`)'
+						. ' (`author`, `last_modified`, `raw_content`, `content`, `page`)'
 						. ' VALUES (?, ?, ?, ?, ?)');
 			} else
 			{
@@ -221,10 +223,10 @@
 									  . ', `last_modified`=?'
 									  . ', `raw_content`=?'
 									  . ', `content`=?'
-									  . ' WHERE `page_name`=?'
+									  . ' WHERE `page`=?'
 									  . ' LIMIT 1');
 			}
-
+			
 			$db->execute($query, $args);
 			$db->free($query);
 		}
