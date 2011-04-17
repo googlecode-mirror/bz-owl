f51b95ce3c60ff2e9d66918fd8176f35429b26ad
toughshooter@gmail.com <toughshooter@gmail.com@68b30ebe-0d82-dabe-3d00-e961b679a46f>
2011-04-17 20:05:38 +0000

    add country maintenance code

--- trunk/leaguesite/web/CMS/add-ons/maintenance/maintenance.php
+++ trunk/leaguesite/web/CMS/add-ons/maintenance/maintenance.php
@@ -71,7 +71,78 @@
 		
 		function updateCountries()
 		{
+			global $db;
 			
+			
+			$query = $db->prepare('SELECT `id` FROM `countries` WHERE `id`=? LIMIT 1');
+			$db->execute($query, '1');
+			$insert_entry = ($db->fetchRow($query) === false);
+			$db->free($query);
+			
+			if ($insert_entry)
+			{
+				$query = $db->prepare('INSERT INTO `countries` (`id`,`name`, `flagfile`) VALUES (?, ?, ?)');
+				$db->execute($query, array('1', 'here be dragons', ''));
+			}
+			
+			$dir = dirname(dirname(dirname(dirname(__FILE__)))) . '/Flags';
+			$countries = array();
+			if ($handle = opendir($dir))
+			{
+				while (false !== ($file = readdir($handle)))
+				{
+					if ($file != '.' && $file != '..' && $file != '.svn' && $file != '.DS_Store')
+					{
+						$countries[] = $file;
+					}
+				}
+				closedir($handle);
+			}
+			
+			$queryFlag = $db->prepare('SELECT `flagfile` FROM `countries` WHERE `name`=?');
+			$queryInsertCountry = $db->prepare('INSERT INTO `countries` (`name`, `flagfile`) VALUES (?, ?)');
+			$queryUpdateCountry = $db->prepare('UPDATE `countries` SET `flagfile`=? WHERE `name`=?');
+			foreach($countries as &$one_country)
+			{
+				$flag_name_stripped = str_replace('Flag_of_', '', $one_country);
+				$flag_name_stripped = str_replace('.png', '', $flag_name_stripped);
+				$flag_name_stripped = str_replace('_', ' ', $flag_name_stripped);
+				
+				// check if flag exists in database
+				$db->execute($queryFlag, $flag_name_stripped);
+				
+				$update_country = false;
+				$insert_entry = false;
+				if (!(mysql_num_rows($result) > 0))
+				{
+					$update_country = true;
+					$insert_entry = true;
+				}
+				if (!$update_country)
+				{
+					while ($row = mysql_fetch_array($result))
+					{
+						if (!(strcmp($row['flagfile'], $one_country) === 0))
+						{
+							$update_country = true;
+						}
+					}
+				}
+				mysql_free_result($result);
+				
+				if ($update_country)
+				{
+					if ($insert_entry)
+					{
+						$db->execute($queryInsertCountry, array($flag_name_stripped, $one_country));
+						$db->free($queryInsertCountry);
+					} else
+					{
+						$db->execute($queryUpdateCountry, array($one_country, $flag_name_stripped));
+						$db->free($queryUpdateCountry);
+					}
+				}
+			}
 		}
 	}
 ?>
