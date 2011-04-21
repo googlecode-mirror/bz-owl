5c5486ae904bd79c3bfc108ff8e3177bb2cca9a6
toughshooter@gmail.com <toughshooter@gmail.com@68b30ebe-0d82-dabe-3d00-e961b679a46f>
2011-04-21 17:10:47 +0000

    delete private messages after user wishes to do so

--- trunk/leaguesite/web/CMS/add-ons/pmSystem/pmDelete.php
+++ trunk/leaguesite/web/CMS/add-ons/pmSystem/pmDelete.php
@@ -5,7 +5,7 @@
 		function __construct($folder, $id)
 		{
 			// get confirmation step info
-			$confirmed = isset($_POST['confirmed']) ? $_POST['confirmed'] : 0;
+			$confirmed = isset($_POST['confirmationStep']) ? $_POST['confirmationStep'] : 0;
 			// run sanity checks
 			$this->sanityCheck($confirmed);
 			
@@ -23,6 +23,40 @@
 			}
 		}
 		
+		
+		function delete($folder, $id)
+		{
+			global $tmpl;
+			global $user;
+			global $db;
+			
+			
+			// just delete it from the user's private message table
+			$query = $db->prepare('DELETE FROM `pmsystem.msg.users`'
+								  . ' WHERE `userid`=:uid AND `msgid`=:msgid LIMIT 1');
+			$params = array(':uid' => array($user->getID(), PDO::PARAM_INT),
+							':msgid' => array($id, PDO::PARAM_INT));
+			$db->execute($query, $params);
+			
+			
+			// check for message usage
+			$query = $db->prepare('SELECT `msgid` FROM `pmsystem.msg.users`'
+								  . ' WHERE `userid`<>:uid AND `msgid`=:msgid LIMIT 1');
+			$db->execute($query, $params);
+			
+			// delete message from storage if no one has the message in mailbox anymore
+			if (($row = $db->fetchRow($query)) === false)
+			{
+				$query = $db->prepare('DELETE FROM `pmsystem.msg.storage` WHERE `msgid`=:msgid');
+				$params = array(':uid' => array($user->getID(), PDO::PARAM_INT));
+				$db->execute($query, $params);
+			}
+			
+			$tmpl->setTemplate('PMDelete');
+			$tmpl->assign('pmDeleted', true);
+		}
+		
+		
 		function preview($folder, $id)
 		{
 			global $tmpl;
@@ -35,9 +69,17 @@
 			$tmpl->assign('title', 'Delete ' . $tmpl->getTemplateVars('title'));
 		}
 		
+		
 		function sanityCheck(&$confirmed)
 		{
-		
+			// we do not need to know if user owns this message
+			// because delete operation can not delete message
+			// from different user
+			
+			if ($confirmed < 0 || $confirmed > 1)
+			{
+				$confirmed = 0;
+			}
 		}
 	}
 ?>
