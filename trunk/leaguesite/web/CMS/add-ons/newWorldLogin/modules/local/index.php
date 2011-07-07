af22fc7f6f4ffd829ef3e0b3c262703d55765300
toughshooter@gmail.com <toughshooter@gmail.com@68b30ebe-0d82-dabe-3d00-e961b679a46f>
2011-07-07 08:59:17 +0000

    fill the new local login module with code for validating login and applying permissions; make clear the old login module is not to be used anymore

--- trunk/leaguesite/web/CMS/add-ons/newWorldLogin/modules/local/index.php
+++ trunk/leaguesite/web/CMS/add-ons/newWorldLogin/modules/local/index.php
@@ -76,6 +76,7 @@
 							 . 'same username or you will give someone else access to your account '
 							 . 'and that access can never be revoked.</span></strong></p>');
 				}
+				unset($modules);
 			}
 			
 			$msg .= 'Enter login data from <strong>' . $oldWebsiteName . '</strong> here!</p>';
@@ -118,9 +119,170 @@
 		}
 		
 		
-		function validateLogin(&$output)
+		public function validateLogin(&$output)
 		{
+			// set password based on POST parameter
+			// no password -> login failed
+			if (!isset($_POST['pw']))
+			{
+				return false;
+			}
+			$pw = $_POST['pw'];
 			
+			// set loginname based on POST parameter
+			// no loginname -> login failed
+			if (!isset($_POST['loginname']))
+			{
+				return false;
+			}
+			$loginname = $_POST['loginname'];
+			
+			// initialise permissions
+			$user->removeAllPermissions();
+			
+			// initialise match variables for password and user
+			$correctUser = false;
+			$correctPw = false;
+			
+			
+			$lenLogin = strlen($loginname);
+			if (($lenLogin > 50) || ($lenLogin < 1))
+			{
+				$output .= 'User names must be using less than 50 '
+						   . 'but more than 0 <abbr title="characters">chars</abbr>.' . "\n");
+				return false;
+			}
+			
+			// get player id
+			$query = 'SELECT `id`';
+			if ($config->getValue('forceExternalLoginOnly'))
+			{
+				$query .= ', `external_id` ';
+			}
+			// only one player tries to login so only fetch one entry, speeds up login a lot
+			$query .= ' FROM `players` WHERE `name`=? LIMIT 1';
+			
+			$query = $db->prepare($query);
+			$db->execute($query, $loginname);
+			
+			
+			// initialise with reserved player id 0 (no player)
+			$playerid = (int) 0;
+			$convert_to_external_login = true;
+			while($row = $db->fetchRow($query))
+			{
+				$playerid = $row['id'];
+				if ($config->getValue('forceExternalLoginOnly') && !(strcmp(($row['external_id']), '') === 0))
+				{
+					$convert_to_external_login = false;
+				}
+			}
+			
+			// local login tried but external login forced in settings
+			if (!$convert_to_external_login && $config->getValue('forceExternalLoginOnly'))
+			{
+				$msg = '<span class="unread_messages">You already enabled ';
+				$modules = newWorldLogin::getModules();
+				if (array_search('bzbb', $modules) !== false)
+				{
+					$url = urlencode($config->getValue('baseaddress') . 'Login/' . '?bzbbauth=%TOKEN%,%USERNAME%');
+					$msg .= '<a href="' . htmlspecialchars('http://my.bzflag.org/weblogin.php?action=weblogin&url=') . $url;						
+					$msg .= '">global (my.bzflag.org/bb/) login</a>';
+				} else
+				{
+					$msg .= 'external logins';
+				}
+				$msg .= ' for this account.</span>' . "\n";
+				$output .= $msg;
+				unset($modules);
+				
+				return false;
+			}
+			
+			
+			if (intval($playerid) === 0)
+			{
+				$user->logout();
+				
+				$output .= ('The specified user is not registered. '
+							. 'You may want to <a href="./">try logging in again</a>.');
+				return false;
+			}
+			
+			// get password from database in order to compare it with the user entered password
+			$query = ('SELECT `password`, `password_encoding` FROM `players_passwords` WHERE `playerid`=?'
+					  // only one player tries to login so only fetch one entry, speeds up login a lot
+					  . ' LIMIT 1');
+			
+			// execute query
+			$query = $db->prepare($query);
+			if (!$db->execute($query, $playerid))
+			{
+				// query failed
+				$output .= 'Could not retrieve password for you in database.';
+				return false;
+			}
+			
+			// initialise without md5 (hash functions could cause collisions despite passwords will not match)
+			$password_md5_encoded = false;
+			// no password is default and will not match any password set
+			$password_database = '';
+			while($row = $db->fetchRow($query))
+			{
+				if (strcmp($row['password_encoding'],'md5') === 0)
+				{
+					$password_md5_encoded = true;
+				}
+				$password_database = $row['password'];
+			}
+			
+			$lenPw = strlen($pw);
+			
+			// webleague imported passwords have unknown length limitations 
+			if (!$password_md5_encoded)
+			{
+				if (($lenPw < 10) || ($lenPw > 32) && (strlen($password_database) !== 0))
+				{
+					$output .= ('Passwords must be using less than 32 but more than 9 '
+								. '<abbr title="characters">chars</abbr>.'
+								. ' You may want to <a href="./">try logging in again</a>.</p>' . "\n");
+					return false;
+				}
+			} else
+			{
+				// generate md5 hash of user entered password
+				$pw = md5($pw);
+			}
+			
+			if (!(strcmp($password_database, $pw) === 0) && strlen($password_database) > 0)
+			{
+				// TODO: automatically log these cases and lock account for some hours after several unsuccessful tries
+				$output .= ('Your password does not match the stored password.'
+							. ' You may want to <a href="./">try logging in again</a>.' . "\n");
+				return false;
+			}
+			
+			// sanity checks passed -> login successful
+			return true;
+			
+			// username and password did match but there might be circumstances
+			// where the caller script decides the login was not successful, though
+		}
+		
+		
+		public function givePermissions()
+		{
+			global $user;
+			
+			
+			// standard permissions for user
+			$_SESSION['username'] = $loginname;
+			$_SESSION['user_logged_in'] = true;
+			$internal_login_id = $playerid;
+			
+			// permissions for private messages
+			$user->setPermission('allow_add_messages');
+			$user->setPermission('allow_delete_messages');
 		}
 	}
 ?>
