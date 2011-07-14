6100bf5095554cb940ae7dea54f322a5a09b3c1f
toughshooter <toughshooter@gmail.com>
2011-07-14 15:36:07 +0000

    add key protection against foreign forms; add code to write changes

--- leaguesite/web/CMS/add-ons/pageSystem/classes/pageOperations.php
+++ leaguesite/web/CMS/add-ons/pageSystem/classes/pageOperations.php
@@ -1,6 +1,74 @@
 <?php
 	class pageOperations extends database
 	{
+		public function changeRequested($id, $requestPath, $title, $addon)
+		{
+			// FIXME: lock CMS table!
+			
+			
+			// check if id set in db
+			// TODO: check if no data change -> abort soon
+			$query = $this->prepare('SELECT `id`,`addon` FROM `CMS` WHERE `id`=? LIMIT 1');
+			$this->execute($query, $id);
+			$row = $this->fetchRow($query);
+			if (!$row)
+			{
+				return 'FATAL ERROR: Change of id ' . $id . ' in pageSystem requested but id not in db.';
+			}
+			$this->free($query);
+			
+			// do not allow to change any entry of pageSystem
+			if (strcmp($row['addon'], 'pageSystem') === 0)
+			{
+				return ('Removing pageSystem from any URL is not supported by this add-on. '
+						.'Reason: It could really confuse people and cause plenty of trouble. '
+						.'If you really want to do this, you must edit db directly instead');
+			}
+			
+			// remove illegal characters from request path
+			if (strlen($requestPath) > strlen($this->cleanRequestPath($requestPath)))
+			{
+				echo('<br><br>' . $requestPath . '<br>' . $this->cleanRequestPath($requestPath));
+				return 'Invalid characters in request path detected. Please check your input.';
+			}
+			
+			// check if request path is unique
+			$query = $this->prepare('SELECT `id` FROM `CMS` WHERE `id`<>? AND `requestPath`=? LIMIT 1');
+			$this->execute($query, array($id, $requestPath));
+			$row = $this->fetchRow($query);
+			$this->free($query);
+			if ($row)
+			{
+				return 'Request path already in use, request path must be unique in db';
+			}
+			
+			// write the changes
+			$query = $this->prepare('UPDATE `CMS` SET `requestPath`=?,`title`=?,`addon`=? WHERE `id`=? LIMIT 1');
+			$this->execute($query, array($requestPath, $title, $addon, $id));
+			
+			return 'Changes written successfully.';
+		}
+		
+		
+		private function cleanRequestPath(&$path)
+		{
+			// TODO: consider using a regular expression to only allow valid chars (whitelist)
+			
+			// remove special chars such as ? from request path
+			$i = strpos($path, '?');
+			// do not remove chars on no match
+			if ($i !== false)
+			{
+				$path = substr($path, 0, $i);
+			}
+			
+			// remove whitespace from the end at least
+			rtrim($path);
+			
+			return $path;
+		}
+		
+		
 		public function getPageList()
 		{
 			// this function collects the list of assigned pages directly from database
@@ -11,6 +79,7 @@
 			return $pages;
 		}
 		
+		
 		public function getPageData($id)
 		{
 			$query = $this->prepare('SELECT `id`,`requestPath`,`title`,`addon` FROM `CMS` WHERE `id`=? LIMIT 1');
@@ -18,6 +87,7 @@
 			
 			return $this->fetchRow($query);
 		}
+		
 		
 		public function hasPermission()
 		{
--- leaguesite/web/CMS/add-ons/pageSystem/pageSystem.php
+++ leaguesite/web/CMS/add-ons/pageSystem/pageSystem.php
@@ -30,14 +30,14 @@
 			$tmpl->assign('title', $title);
 			
 			
-			
-			if (!isset($_GET['action']) || count($_POST) < 1)
+			// show overview by default if no action specified, post parameters missing or cancel requested
+			if (!isset($_GET['action']) || count($_POST) < 1 || isset($_POST['cancel']))
 			{
-				$pages = $this->pageOperations->getPageList();
-				$tmpl->assign('pageList', $pages);
+				$this->showOverview();
 				die();
 			}
 			
+			// call the event handlers to process the requested task
 			switch($_GET['action'])
 			{
 				case 'change':
@@ -51,29 +51,79 @@
 			
 		}
 		
-		private function handleChangePageRequest()
+		
+		private function showOverview()
 		{
 			global $tmpl;
 			
 			
-			// tell template that we're trying to edit page assignment data
-			$tmpl->assign('pageChange', true);
+			$pages = $this->pageOperations->getPageList();
+			$tmpl->assign('pageList', $pages);
+		}
+		
+		
+		private function handleChangePageRequest()
+		{
+			global $tmpl;
+			
 			
 			// grab the first element of POST array by using each
 			$operation = each($_POST);
 			// reset array pointer that was changed to next element by using each
 			reset($_POST);
 			
-			if (count(strval($operation[0])) > 1000000)
+			if (!isset($_POST['key_name']) && count(strval($operation[0])) > 1000000)
 			{
 				echo('either you use way too much pages for this CMS or you tried to break the pageSystem');
 				die();
 			}
 			
-			// see if first part of request is equal change (removing the number)
+			// check validity of data if write change requested
+			if (isset($_POST['change'])
+			&& isset($_POST['requestPath'])
+			&& isset($_POST['title'])
+			&& isset($_POST['addon']))
+			{
+				// initialise guards
+				$keyName = '';
+				$keyValue = '';
+				
+				if (isset($_POST['key_name']))
+				{
+					$keyName = $_POST['key_name'];
+				}
+				
+				if (isset($_POST[$keyName]))
+				{
+					$keyValue = $_POST[$keyName];
+				}
+				
+				if (strlen($keyName) > 0
+					&& strlen($keyValue) > 0
+					&& $this->validateKey($keyName, $keyValue))
+				{
+					// pass this to the pageOperations class which will do db sanity checks and apply changes
+					// it needs id, request path, title and add-on to be used
+					echo ($this->pageOperations->changeRequested($_POST['change'],
+																 $_POST['requestPath'],
+																 $_POST['title'],
+																 $_POST['addon']));
+				} else
+				{
+					echo('error: request illegal: Could not confirm key data');
+				}
+				$this->showOverview();
+				die();
+				
+			}
+			
+			// write request logic finished
+			// now back to form request logic
+			
+			// see if first part of request is equal change (removing the number) if form requested
 			if (substr_compare(strval($operation[0]), 'change', 0, 6))
 			{
-				echo('error: request illegal');
+				echo('error: request illegal:' . strval($operation[0]));
 				die();
 			}
 			
@@ -81,7 +131,7 @@
 			// get id in question from request string
 			$id = intval(substr(strval($operation[0]), 6));
 			
-			// TODO: eek, count is slow! Ask db about size instead.
+			// TODO: eek, count is slow and insecure in this context! Ask db whether entry exists instead.
 			$pages = $this->pageOperations->getPageList();
 			if (count($pages) < $id)
 			{
@@ -89,8 +139,35 @@
 				die();
 			}
 			
+			// give template information about page assignement in question
 			$pageData = $this->pageOperations->getPageData($id);
 			$tmpl->assign('pageData', $pageData);
+			
+			// tell template that we're trying to edit page assignment data
+			$tmpl->assign('pageChange', true);
+			
+			// generate random keys to avoid changing data through accidental clicking on links
+			$this->generateKey();
+		}
+		
+		private function generateKey()
+		{
+			global $site;
+			global $tmpl;
+			
+			$randomKeyName = 'addon.pageSystem.' . microtime();
+			// convert some special chars to underscores
+			$randomKeyName = strtr($randomKeyName, array(' ' => '_', '.' => '_'));
+			$randomkeyValue = $site->setKey($randomKeyName);
+			$tmpl->assign('keyName', $randomKeyName);
+			$tmpl->assign('keyValue', htmlent($randomkeyValue));
+		}
+		
+		private function validateKey($key, $value)
+		{
+			global $site;
+			
+			return $site->validateKey($key, $value);
 		}
 		
 		public function __destruct()
