1ee98bf10461dda08b990ca232ee9e2b35bd9837
toughshooter <toughshooter@gmail.com>
2011-07-14 09:51:52 +0000

    use a seperate section in template for the actual page change form; do some sanity checks on the input data

--- leaguesite/web/CMS/add-ons/pageSystem/classes/pageOperations.php
+++ leaguesite/web/CMS/add-ons/pageSystem/classes/pageOperations.php
@@ -5,10 +5,32 @@
 		{
 			// this function collects the list of assigned pages directly from database
 			
-			$query = $this->SQL('SELECT `requestPath`,`title`,`addon` FROM `CMS` ORDER BY `id`');
+			$query = $this->SQL('SELECT `id`,`requestPath`,`title`,`addon` FROM `CMS` ORDER BY `id`');
 			$pages = $this->fetchAll($query);
 			
 			return $pages;
+		}
+		
+		public function getPageData($id)
+		{
+			$query = $this->prepare('SELECT `id`,`requestPath`,`title`,`addon` FROM `CMS` WHERE `id`=? LIMIT 1');
+			$this->execute($query, $id);
+			
+			return $this->fetchRow($query);
+		}
+		
+		public function hasPermission()
+		{
+			global $user;
+			global $config;
+			
+			
+			if (!$config->getValue('debugSQL') && !$user->getPermission('allow_admin_pageSystem'))
+			{
+				return false;
+			}
+			
+			return true;
 		}
 	}
 ?>
--- leaguesite/web/CMS/add-ons/pageSystem/pageSystem.php
+++ leaguesite/web/CMS/add-ons/pageSystem/pageSystem.php
@@ -1,35 +1,106 @@
 <?php
 	class pageSystem
 	{
+		private $hasPermission = false;
+		private $pageOperations;
+		
+		
 		public function __construct($title)
 		{
 			global $config;
+			global $user;
 			global $tmpl;
 			global $db;
 			
 			
-			$tmpl->assign('title', $title);
+			// setup support class
+			require(dirname(__FILE__) . '/classes/pageOperations.php');
+			$this->pageOperations = new pageOperations();
 			
-			// FIXME: check for permission!!
-			if (!$config->getValue('debugSQL'))
+			
+			// FIXME: check for permission only in final version!!
+			$this->hasPermission = $this->pageOperations->hasPermission();
+			if (!$this->hasPermission)
 			{
 				$tmpl->display('NoPerm');
 				die();
 			}
 			
-			
-			// setup support class
-			require(dirname(__FILE__) . '/classes/pageOperations.php');
-			$pageOperations = new pageOperations();
+			// set title
+			$tmpl->assign('title', $title);
 			
 			
-			if (!isset($_GET['action']))
+			
+			if (!isset($_GET['action']) || count($_POST) < 1)
 			{
-				$pages = $pageOperations->getPageList();
+				$pages = $this->pageOperations->getPageList();
+				$tmpl->assign('pageList', $pages);
+				die();
 			}
-			$tmpl->assign('pages', $pages);
 			
-			$tmpl->display('pageSystem');
+			switch($_GET['action'])
+			{
+				case 'change':
+					$this->handleChangePageRequest();
+					break;
+				
+				// do nothing by default
+				default:
+					die();
+			}
+			
+		}
+		
+		private function handleChangePageRequest()
+		{
+			global $tmpl;
+			
+			
+			// tell template that we're trying to edit page assignment data
+			$tmpl->assign('pageChange', true);
+			
+			// grab the first element of POST array by using each
+			$operation = each($_POST);
+			// reset array pointer that was changed to next element by using each
+			reset($_POST);
+			
+			if (count(strval($operation[0])) > 1000000)
+			{
+				echo('either you use way too much pages for this CMS or you tried to break the pageSystem');
+				die();
+			}
+			
+			// see if first part of request is equal change (removing the number)
+			if (substr_compare(strval($operation[0]), 'change', 0, 6))
+			{
+				echo('error: request illegal');
+				die();
+			}
+			
+			
+			// get id in question from request string
+			$id = intval(substr(strval($operation[0]), 6));
+			
+			// TODO: eek, count is slow! Ask db about size instead.
+			$pages = $this->pageOperations->getPageList();
+			if (count($pages) < $id)
+			{
+				echo('error: request was to change higher entry than existing');
+				die();
+			}
+			
+			$pageData = $this->pageOperations->getPageData($id);
+			$tmpl->assign('pageData', $pageData);
+		}
+		
+		public function __destruct()
+		{
+			global $tmpl;
+			
+			if (isset($this->hasPermission))
+			{
+				$tmpl->display('pageSystem');
+			}
 		}
 	}
 ?>
